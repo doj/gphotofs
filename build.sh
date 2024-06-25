47b970bd59a6974481ddb5c8aa4d6679e5dbc7bd
Sau Chow <sauchow@sandiego.edu>
2024-06-25 21:25:48 +0000

    check for fuse3 in autoconf, add a build.sh script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# build the photofs code from a github checkout
+
+if [ "$1" = clean -o "$1" = distclean ] ; then
+    make distclean
+    exit 0
+fi
+
+set -e
+autoreconf -i
+./configure
+make
